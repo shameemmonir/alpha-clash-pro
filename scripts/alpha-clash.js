09125e1a6a3f03628083acf95fa157a5a81cbca5
shameemmonir <shameemmonir@gmail.com>
2024-06-06 20:07:26 +0600

    hidden home screen show play ground section

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,14 @@
+// function play(){
+//     const homeSection = document.getElementById ('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log (homeSection.classList)
+
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     // console.log(playgroundSection.classList);
+// }
+
 function play(){
-    console.log('play start now');
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
